a3be03ea4e50bbf3752960fd0ac2d0924b399b7c
adriyanaaa <165284553+adriyanaaa@users.noreply.github.com>
2024-11-23 15:49:59 +0200

    Create main.cpp

---
+++ Divide-et-impera/TA-problema/main.cpp
@@ -0,0 +1,5 @@
+// Fie un vect cu n elem int. Se cere:
+// a) produsul elementelor care au ult cifra nenula
+// b) sa se afle daca vectorul este descresc
+// c) care e cel mai mic elem din vect? dar din prima jum?
+
